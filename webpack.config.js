e6dfa7c1f52b38c378e389be1cdd5b85796dc063
trazeygit <jacotygraves2006@gmail.com>
2026-02-06 16:00:08 -0600

    Add initial webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: {
+      type: 'module',
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  mode: 'production',
+};
